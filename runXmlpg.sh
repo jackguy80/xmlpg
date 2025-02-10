b81420aa39a42ca1e5a29f773bd9ef770b3ed47a
Jack <jack.hopgood1@gmail.com>
2025-02-10 17:31:14 +1030

    Update shell script to mirror batch.

--- runXmlpg.sh
+++ runXmlpg.sh
@@ -1,13 +1,8 @@
 # Example for running xmlpg and generating code for DIS protocol.
 # This uses exec-maven-plugin to run, but you could run from xmlpg.jar too.
 
-#schema=../DISDescription/DIS6.xml
-schema=DIS2012.xml
-
 mvn clean compile
 
-for lang in java cpp python javascript objc csharp; do
+mvn exec:java -Dexec.mainClass=edu.nps.moves.xmlpg.Xmlpg  -Dxmlpg.generatedSourceDir=target/generated-sources/xmlpg-DIS6-cpp/ -Dexec.args="DIS6.xml cpp"
+mvn exec:java -Dexec.mainClass=edu.nps.moves.xmlpg.Xmlpg  -Dxmlpg.generatedSourceDir=target/generated-sources/xmlpg-DIS7-cpp/ -Dexec.args="DIS7.xml cpp"
 
-mvn exec:java -Dexec.mainClass=edu.nps.moves.xmlpg.Xmlpg  -Dxmlpg.generatedSourceDir=target/generated-sources/xmlpg-`basename $schema .xml`-$lang/ -Dexec.args="$schema $lang"
-
-done;
